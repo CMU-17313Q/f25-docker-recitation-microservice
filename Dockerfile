6ce2395ca7c0cf1c365e12fa2b57f6dbcfd19aee
EduardoFF <efeoflus@andrew.cmu.edu>
2025-11-08 22:46:19 +0300

    added dockerfile and app for f25

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,12 @@ FROM python:3.9
 
 # specify the working directory for the image
 WORKDIR /code
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
-# TODO
+# copies the code from your host machine to the image
+COPY . .
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -8,38 +8,29 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-#TODO Fix recitation hours to be correct for this semester.
-RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
-                    "c": "11:00~11:50", "d": "12:00~12:50"}
-MICROSERVICE_LINK = "http://17313-teachers2.s3d.cmu.edu:8080/section_info/"
+
+TEAM_NAMES = {"1": "nodegpt", "2": "bitwise",
+              "3": "gituardo", "4": "sickfault",
+              "5": "debug-divas", "6": "codex"}
+#MICROSERVICE_LINK = "http://17313-teachers2.s3d.cmu.edu:8080/section_info/"
+
+TEAM_MENTORS = {"1": "Seckhen", "2": "Aadi",
+              "3": "Steve", "4": "Seckhen",
+              "5": "Aadi", "6": "Steve"}
+
+@app.get("/team_name/{team_id}")
+def get_team_name(team_id: str):
+
+    if team_id is None:
+        raise HTTPException(status_code=404, detail="Missing team id")
+
+    team_id = team_id.lower()
 
 
-@app.get("/section_info/{section_id}")
-def get_section_info(section_id: str):
-
-    if section_id is None:
-        raise HTTPException(status_code=404, detail="Missing section id")
-
-    section_id = section_id.lower()
-
-    response = requests.get(MICROSERVICE_LINK + section_id)
-
-    # You can check out what the response body looks like in terminal using the print statement
-    data = response.json()
-    print(data)
-    ta_name_list = data["ta"]
-    ta1_name = ta_name_list[0]
-    ta2_name = ta_name_list[1]
-
-    print(ta1_name)
-
-    # TODO Fix this to return correct values for correct sections.
-    if section_id == "a":
+    # TODO Fix this to return correct values for correct team ids.
+    if team_id in TEAM_NAMES:
         return {
-            "section": "section_name",
-            "start_time": "HH:MM",
-            "end_time": "HH:MM",
-            "ta": ["taName1", "taName2"]
+            "team_name": TEAM_NAMES[team_id]
         }
     else:
-        raise HTTPException(status_code=404, detail="Invalid section id")
+        raise HTTPException(status_code=404, detail="Invalid team id")
